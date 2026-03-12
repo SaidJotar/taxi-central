dbe49c293813534d5a3900a81b895ef0f78087b5
SaidJotar <jotar.teleco@gmail.com>
2026-03-12 10:25:14 +0100

    Eliminar log DATABASE_URL

--- src/services/bd.js
+++ src/services/bd.js
@@ -7,8 +7,6 @@ if (!process.env.DATABASE_URL) {
   throw new Error("DATABASE_URL no está definida en el entorno");
 }
 
-console.log("DATABASE_URL cargada:", process.env.DATABASE_URL);
-
 const adapter = new PrismaPg({
   connectionString: process.env.DATABASE_URL,
 });
